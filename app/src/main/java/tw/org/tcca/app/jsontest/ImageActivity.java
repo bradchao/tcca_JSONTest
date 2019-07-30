6580ede028a8120ed4aea08c0f585cd3d8cf90e6
Brad <brad@brad.tw>
2019-07-30 20:51:48 +0800

    08

--- app/src/main/java/tw/org/tcca/app/jsontest/ImageActivity.java
+++ app/src/main/java/tw/org/tcca/app/jsontest/ImageActivity.java
@@ -1,13 +1,22 @@
 package tw.org.tcca.app.jsontest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.ImageView;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ImageActivity extends AppCompatActivity {
-    private String url;
+    private String url = null;
     private ImageView img;
+    private Bitmap bmp = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +33,29 @@ public class ImageActivity extends AppCompatActivity {
         new Thread(){
             @Override
             public void run() {
+                try {
+                    URL url = new URL(url);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
 
+                    bmp = BitmapFactory.decodeStream(conn.getInputStream());
+                    
+
+                }catch (Exception e){
+
+                }
             }
         }.start();
+    }
+
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            img.setImageBitmap(bmp);
+        }
     }
 
 }
