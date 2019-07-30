e606e7764c73dbac8a7ca9b71c8545751ae23fb7
Brad <brad@brad.tw>
2019-07-30 20:58:33 +0800

    09

--- app/src/main/java/tw/org/tcca/app/jsontest/ImageActivity.java
+++ app/src/main/java/tw/org/tcca/app/jsontest/ImageActivity.java
@@ -8,6 +8,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.widget.ImageView;
 
 import java.net.HttpURLConnection;
@@ -17,32 +18,34 @@ public class ImageActivity extends AppCompatActivity {
     private String url = null;
     private ImageView img;
     private Bitmap bmp = null;
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
 
+        handler = new UIHandler();
         img = findViewById(R.id.img);
         url = getIntent().getStringExtra("pic");
-        fetchImageData();
+        fetchImageData(url);
 
     }
 
-    private void fetchImageData(){
+    private void fetchImageData(final String imgurl){
         new Thread(){
             @Override
             public void run() {
                 try {
-                    URL url = new URL(url);
+                    URL url = new URL(imgurl);
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.connect();
 
                     bmp = BitmapFactory.decodeStream(conn.getInputStream());
-                    
+                    handler.sendEmptyMessage(0);
 
                 }catch (Exception e){
-
+                    Log.v("brad", e.toString());
                 }
             }
         }.start();
